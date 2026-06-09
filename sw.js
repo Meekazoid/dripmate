0d2466bc1cf1cb509151d7ac285674e38143120b
Meekazoid <hpfahl1@gmail.com>
2026-06-09 21:17:03 +0200

    Update CACHE_VERSION to v58.2

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Provides offline PWA support with intelligent caching strategies
 */
 
-const CACHE_VERSION = 'v58.1';
+const CACHE_VERSION = 'v58.2';
 
 // Static assets to pre-cache during installation
 const STATIC_ASSETS = [
