d6d8c73c03d423c73f9d9111222d7dfece0bb1f9
Meekazoid <hpfahl1@gmail.com>
2026-06-12 15:18:08 +0200

    sw

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Provides offline PWA support with intelligent caching strategies
 */
 
-const CACHE_VERSION = 'v59.4';
+const CACHE_VERSION = 'v59.5';
 
 // Static assets to pre-cache during installation
 const STATIC_ASSETS = [
