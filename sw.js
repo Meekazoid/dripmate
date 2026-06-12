a93f9f7967aad23fa98c9b816e31957c08a86498
Meekazoid <hpfahl1@gmail.com>
2026-06-13 00:13:05 +0200

    buttonResetFinish_fix

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Provides offline PWA support with intelligent caching strategies
 */
 
-const CACHE_VERSION = 'v59.9';
+const CACHE_VERSION = 'v60.0';
 
 // Static assets to pre-cache during installation
 const STATIC_ASSETS = [
